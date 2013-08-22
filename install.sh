c925afb96088dc6500630b09683e89fd295ba7d6
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-23 01:39:34 +1000

    fix old file move command

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ echo "done"
 # in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv -rf ~/.$file ~/dotfiles_old/
+    mv -f ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
