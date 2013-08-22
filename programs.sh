26ab79fa679782d7aa60a4fdc750e99ec9b0ad84
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-23 02:44:08 +1000

    added meat to the programs.sh

--- programs.sh
+++ programs.sh
@@ -4,3 +4,18 @@
 # This script installs some handy programs
 ############################
 
+PROGRAMS="emacs mc python python-pip python-setuptools"
+
+haveProg() {
+    [ -x "$(which $1)" ]
+}
+
+if haveProg apt-get ; then $PACKER="apt-get install"
+elif haveProg pacman ; then $PACKER="pacman -S"
+else
+    echo "What command do you use to install packages? Including options?"
+    read PACKER
+    if [ -z $PACKER ]; then exit 2; fi
+fi
+
+$PACKER $PROGRAMS
