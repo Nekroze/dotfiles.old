6e89b9bb8756d5967d23bd6c61389e95c017fed6
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-23 02:53:04 +1000

    removed broken package manager detector

--- programs.sh
+++ programs.sh
@@ -6,16 +6,8 @@
 
 PROGRAMS="emacs mc python python-pip python-setuptools"
 
-haveProg() {
-    [ -x "$(which $1)" ]
-}
-
-if haveProg apt-get ; then $PACKER="apt-get install"
-elif haveProg pacman ; then $PACKER="pacman -S"
-else
-    echo "What command do you use to install packages? Including options?"
-    read PACKER
-    if [ -z $PACKER ]; then exit 2; fi
-fi
+echo "What command do you use to install packages? Including options?"
+read PACKER
+if [ -z $PACKER ]; then exit 2; fi
 
 $PACKER $PROGRAMS
