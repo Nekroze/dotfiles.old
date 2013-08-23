7c3c706efd7a66d3ea47b92b7c760e87a04bc515
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-23 11:53:57 +1000

    reordered zsh setup

--- install.sh
+++ install.sh
@@ -9,7 +9,17 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc zshrc oh-my-zsh config/terminator/config"
+files="bashrc zshrc config/terminator/config"
+
+########## Manage directories
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory ..."
+cd $dir
 
 ########## Setup zsh and oh-my-zsh
 
@@ -20,6 +30,8 @@ if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     if [[ ! -d $dir/oh-my-zsh/ ]]; then
         git clone https://github.com/robbyrussell/oh-my-zsh.git
         cp -f eturnilnetwork.zsh-theme oh-my-zsh/custom/
+        mv -f ~/.oh-my-zsh ~/dotfiles_old
+        mv oh-my-zsh ~/.oh-my-zsh
     fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
@@ -27,20 +39,13 @@ if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     fi
 fi
 
-########## Move files
-
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory ..."
-cd $dir
+########## Copy files
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create
 # symlinks from the homedir to any files in the ~/dotfiles directory specified
 # in $files
 mkdir -p ~/.config/terminator
+mkdir -p ~/dotfiles_old/.config/terminator
 echo "Backing up any existing dotfiles $olddir and replacing symlinks"
 for file in $files; do
     mv -f ~/.$file ~/dotfiles_old/
