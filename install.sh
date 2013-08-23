4042451ffedf66709da97af5005078d0e8f4582c
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-23 11:24:08 +1000

    fix for config folder removing entire old config instead of just terminator config

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="bashrc zshrc oh-my-zsh config"
+files="bashrc zshrc oh-my-zsh config/terminator/config"
 
 ########## Setup zsh and oh-my-zsh
 
@@ -31,17 +31,18 @@ fi
 ########## Move files
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+echo "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
 
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
+echo "Changing to the $dir directory ..."
 cd $dir
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create
 # symlinks from the homedir to any files in the ~/dotfiles directory specified
 # in $files
-echo "Backing up any existing dotfiles in ~ to $olddir and replacing with dotfiles symlinks"
+mkdir -p ~/.config/terminator
+echo "Backing up any existing dotfiles $olddir and replacing symlinks"
 for file in $files; do
     mv -f ~/.$file ~/dotfiles_old/
     echo "Linking ~/.$file"
