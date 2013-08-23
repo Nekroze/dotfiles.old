22eb29054e87a2aaa00440a8316dffd1c1cef0d9
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-23 12:42:45 +1000

    added extra line before program install

--- programs.sh
+++ programs.sh
@@ -6,6 +6,7 @@
 
 PROGRAMS="emacs mc python python-pip python-setuptools"
 
+echo
 echo "What command do you use to install packages? Including options?"
 read PACKER
 if [[ -z $PACKER ]]; then exit 2; fi
