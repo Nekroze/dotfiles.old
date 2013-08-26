b03cae8a3806023e05a9102fa9b7475c32d7ee01
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-26 16:48:10 +1000

    added python packages to programs to install

--- programs.sh
+++ programs.sh
@@ -5,6 +5,7 @@
 ############################
 
 PROGRAMS="emacs mc python python-pip python-setuptools"
+PYPACKAGES="cookiecutter"
 
 echo
 echo "What command do you use to install packages? Including options?"
@@ -12,3 +13,11 @@ read PACKER
 if [[ -z $PACKER ]]; then exit 2; fi
 
 exec $PACKER $PROGRAMS
+
+echo "Do you need to use sudo to install things?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) exec sudo pip install $PYPACKAGES; break;;
+        No ) exec pip install $PYPACKAGES; break;;
+    esac
+done
