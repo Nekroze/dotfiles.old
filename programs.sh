c15664e4a7343a58a9b7a09f54d7dac49934e302
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-28 00:50:34 +1000

    Added more python packages and opt out for pip installing

--- programs.sh
+++ programs.sh
@@ -4,20 +4,23 @@
 # This script installs some handy programs
 ############################
 
-PROGRAMS="emacs mc python python-pip python-setuptools"
-PYPACKAGES="cookiecutter"
+PROGRAMS="emacs mc python python-pip python-setuptools python-tox python-sphinx
+python-pytest"
+PYPACKAGES="cookiecutter flake8 pylint"
 
-echo
+echo "\nInstalling Software Packages"
 echo "What command do you use to install packages? Including options?"
 read PACKER
 if [[ -z $PACKER ]]; then exit 2; fi
 
 exec $PACKER $PROGRAMS
 
+echo "\nInstalling Python Packages"
 echo "Do you need to use sudo to install things?"
-select yn in "Yes" "No"; do
+select yn in "Yes" "No" "Skip"; do
     case $yn in
         Yes ) exec sudo pip install $PYPACKAGES; break;;
         No ) exec pip install $PYPACKAGES; break;;
+        Skip ) break;;
     esac
 done
