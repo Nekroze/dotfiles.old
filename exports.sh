e908fd64a17af0a1e6d7dea6efe95a3345f13489
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-28 17:37:24 +1000

    Fixed .editor loader location in export to EDITOR variable

--- exports.sh
+++ exports.sh
@@ -3,5 +3,5 @@
 export RPTRANSLATE=~/src/pypy/pypy/translator/goal/translate.py
 export RPYTHON="PYTHONPATH=~/src/pypy:$PYTHONPATH pypy $RPTRANSLATE"
 ## Editor
-export EDITOR=~/dotfiles/.editor
+export EDITOR=~/.editor
 export VISUAL=$EDITOR
