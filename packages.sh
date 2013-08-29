18aed86e7e6686e720d2639e491e1d1e28091be9
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-29 17:22:21 +1000

    Moved new line echoing to a seperate echo

--- packages.sh
+++ packages.sh
@@ -6,15 +6,15 @@
 
 PROGRAMS="emacs24 terminator zsh aspell-en mc python python-pip python-setuptools python-tox python-sphinx python-pytest"
 PYPACKAGES="cookiecutter flake8 pylint"
-
-echo "\nInstalling Software Packages"
+echo
+echo "Installing Software Packages"
 echo "What command do you use to install packages? Including options?"
 read PACKER
 if [[ -z $PACKER ]]; then exit 2; fi
 
 exec $PACKER $PROGRAMS
-
-echo "\nInstalling Python Packages"
+echo
+echo "Installing Python Packages"
 echo "Do you need to use sudo to install things?"
 select yn in "Yes" "No" "Skip"; do
     case $yn in
