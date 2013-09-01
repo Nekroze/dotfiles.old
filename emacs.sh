5160f7c908f6d8549742d63c85353f91769c5146
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-09-02 07:00:28 +1000

    bytecompile emacs.el after installing packages

--- emacs.sh
+++ emacs.sh
@@ -1,11 +1,10 @@
 if type "emacs" > /dev/null; then
     echo
-    echo "Byte compiling emacs config"
-    emacs --batch --eval '(byte-compile-file "~/dotfiles/emacs.el")'
-
-    echo
     echo "Installing emacs packages"
     emacs -l ~/dotfiles/emacspacks.el --batch
+    echo
+    echo "Byte compiling emacs config"
+    emacs --batch --eval '(byte-compile-file "~/dotfiles/emacs.el")'
 else
     echo
     echo "emacs not found!"
