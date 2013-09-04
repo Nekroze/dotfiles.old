9e5ea6721f97d3f7d4cb67d9412e20ccc20dc71f
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-09-05 03:08:22 +1000

    Use propper dir and olddir variables when they should be used

--- install.sh
+++ install.sh
@@ -27,43 +27,43 @@ if [[ ! -d $dir/oh-my-zsh/ ]]; then
     echo "Installing oh-my-zsh with custom theme"
     git clone https://github.com/robbyrussell/oh-my-zsh.git
     cp -f eturnilnetwork.zsh-theme oh-my-zsh/custom/
-    mv -f ~/.oh-my-zsh ~/dotfiles_old
+    mv -f ~/.oh-my-zsh $olddir
     mv oh-my-zsh ~/.oh-my-zsh
 fi
 
 ########## Copy files
 
 echo "Installing terminator config"
-mkdir -p ~/.config ~/dotfiles_old/.config
-mv -f ~/.config/terminator ~/dotfiles_old/.config/terminator
-cp -r ~/dotfiles/config/terminator ~/.config/
+mkdir -p ~/.config $olddir/.config
+mv -f ~/.config/terminator $olddir/.config/terminator
+cp -r $dir/config/terminator ~/.config/
 
 echo "Installing bash config"
-mv -f ~/.bashrc ~/dotfiles_old/
+mv -f ~/.bashrc $olddir
 echo "source ~/dotfiles/bash.sh
 source ~/dotfiles/shellenv.sh" > ~/.bashrc
 
 echo "Installing zsh config"
-mv -f ~/.zshrc ~/dotfiles_old/
+mv -f ~/.zshrc $olddir
 echo "source ~/dotfiles/zsh.sh
 source ~/dotfiles/shellenv.sh" > ~/.zshrc
 # Fix for history completion in ubuntu/debian
 echo "DEBIAN_PREVENT_KEYBOARD_CHANGES=yes" >> ~/.zshenv
 
 echo "Installing emacs config"
-mv -f ~/.emacs ~/dotfiles_old/
+mv -f ~/.emacs $olddir
 echo "(load-file \"~/dotfiles/emacs.el\")" > ~/.emacs
 
 ########## Optional extras
 if [ $QUITE = TRUE ]; then
-    QUITE=$QUITE bash ~/dotfiles/gitsetup.sh
-    QUITE=$QUITE bash ~/dotfiles/emacs.sh
+    QUITE=$QUITE bash $dir/gitsetup.sh
+    QUITE=$QUITE bash $dir/emacs.sh
 else
     echo
     echo "Do you wish to apply some useful global git configs?"
     select yn in "Yes" "No"; do
         case $yn in
-            Yes ) bash ~/dotfiles/gitsetup.sh; break;;
+            Yes ) bash $dir/gitsetup.sh; break;;
             No ) break;;
         esac
     done
@@ -72,7 +72,7 @@ else
     echo "Do you wish to install some useful packages?"
     select yn in "Yes" "No"; do
         case $yn in
-            Yes ) bash ~/dotfiles/packages.sh; break;;
+            Yes ) bash $dir/packages.sh; break;;
             No ) break;;
         esac
     done
@@ -81,7 +81,7 @@ else
     echo "Do you wish to install some useful emacs plugins?"
     select yn in "Yes" "No"; do
         case $yn in
-            Yes ) bash ~/dotfiles/emacs.sh; break;;
+            Yes ) bash $dir/emacs.sh; break;;
             No ) break;;
         esac
     done
