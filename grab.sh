70e364ebf8352977a566fb03cc874bf12c78a903
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-09-05 02:38:42 +1000

    A method for installing in one command

---
+++ grab.sh
@@ -0,0 +1,13 @@
+if [ -d ~/dotfiles ]
+then
+  echo "\033[0;33mYou already have dotfiles installed.\033[0m You'll need to remove ~/dotfiles if you want to install"
+  exit
+fi
+
+echo "\033[0;34mCloning dotfiles...\033[0m"
+hash git >/dev/null && /usr/bin/env git clone https://github.com/Nekroze/dotfiles.git ~/dotfiles || {
+  echo "git not installed"
+  exit
+}
+
+/usr/bin/env bash ~/dotfiles/install.sh
