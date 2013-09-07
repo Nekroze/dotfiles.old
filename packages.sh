ded139084952ce36f245aadf2e6e72c4dafaca34
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-09-07 15:03:11 +1000

    Added better fallbacks for package installation

--- packages.sh
+++ packages.sh
@@ -6,8 +6,6 @@
 
 PROGRAMS="emacs24 terminator zsh aspell-en mc python python-pip python-setuptools python-tox python-sphinx python-pytest python-dev pyflakes pep8 pylint python-tox"
 PYPACKAGES="cookiecutter flake8"
-echo
-echo "Installing Software Packages"
 
 if hash apt-get >/dev/null; then
     echo "apt-get detected"
@@ -24,19 +22,27 @@ elif [ "$QUITE" != "TRUE" ]; then
     if hash $PACKER >/dev/null; then
         exit 2;
     fi
+else
+    PACKER="NONE"
 fi
 
-if hash sudo >/dev/null; then
-    exec sudo $PACKER $PROGRAMS
-else
-    exec sudo $PACKER $PROGRAMS
+if [ "$PACKER" != "NONE" ]; then
+    echo
+    echo "Installing Software Packages"
+    if hash sudo >/dev/null; then
+        exec sudo $PACKER $PROGRAMS
+    else
+        exec sudo $PACKER $PROGRAMS
+    fi
 fi
 
-echo 
-echo "Installing python packages"
-if hash sudo >/dev/null; then
-    echo "sudo detected"
-    exec sudo pip install $PYPACKAGES
-else
-    exec pip install $PYPACKAGES
+if hash pip >/dev/null; then
+    echo 
+    echo "Installing python packages"
+    if hash sudo >/dev/null; then
+        echo "sudo detected"
+        exec sudo pip install $PYPACKAGES
+    else
+        exec pip install $PYPACKAGES
+    fi
 fi
