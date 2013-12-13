200da1b2c1746eb250fbb9a32a2a47870f473a26
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-12-13 19:20:01 +1000

    started implementing new dotfiles python script

---
+++ dotfiles.py
@@ -0,0 +1,46 @@
+#!/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+from __future__ import print_function
+__doc__ = """
+Installation script for Nekroze's dotfiles package.
+"""
+from subprocess import call
+from os import path
+
+try:
+    raw_input
+except NameError:
+    raw_input = input
+
+
+USERHOME = path.expanduser("~")
+DOTDIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def ask_perform(description, commands):
+    """
+    Displays the description to the user and offers the choice of executing the
+    given commands using the `perform` function. If no user input is given it
+    will be considered a yes.
+    """
+    print(description)
+    answer = raw_input("[Y/n]>")
+    if not answer or (isinstance(answer, str) and answer.lower() in ("y", "yes")):
+        perform(commands)
+
+
+def perform(commands):
+    """
+    Executes the given commands each on the shell. Commands may be provided as
+    a list of strings or a single string which will be split into a list of 
+    strings for each line.
+    """
+    if isinstance(commands, str):
+        commands = commands.split()
+    for command in commands:
+        if call(commands.split()):
+            print("<<<<ERROR>>>>")
+            print("<<<<The following command returned non-zero:")
+            print(command)
+            exit()
+
