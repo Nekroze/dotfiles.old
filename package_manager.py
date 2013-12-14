0cc2abbf28dd83ea25983b16445276d508af9f34
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-12-14 11:05:32 +1000

    started to implement the package install system

--- package_manager.py
+++ package_manager.py
@@ -2,7 +2,8 @@
 # -*- mode: python; coding: utf-8 -*-
 from __future__ import print_function
 __doc__ = """Attempt at a universal package manager wrapper."""
-from subprocess import call
+from subprocess import call, check_output
+from os import environ
 import json
 
 
@@ -18,10 +19,31 @@ def detect_package_manager():
 
     for manager in managers:
         if command_exists(manager):
-            prefix = "sudo " if command_exists("sudo") else ""
-            environ["PACKUPDATE"] = prefix + managers[manager]["update"]
-            environ["PACKUPGRADE"] = prefix + managers[manager]["upgrade"]
-            environ["PACKINSTALL"] = prefix + managers[manager]["install"]
-            environ["PACKREMOVE"] = prefix + managers[manager]["remove"]
-            environ["PACKCLEAN"] = prefix + managers[manager]["clean"]
-            break
+            environ["PACKUPDATE"] = managers[manager]["update"]
+            environ["PACKUPGRADE"] = managers[manager]["upgrade"]
+            environ["PACKSEARCH"] = managers[manager]["search"]
+            environ["PACKINSTALL"] = managers[manager]["install"]
+            environ["PACKREMOVE"] = managers[manager]["remove"]
+            environ["PACKCLEAN"] = managers[manager]["clean"]
+            return managers[manager]
+
+
+def attempt_install(packages, manager):
+    for package in packages:
+        output = check_output("{0} {1}".format(manager["search"], package).split())
+        pattern = manager["pattern"].format(package)
+        for line in output.split("\n"):
+            if len(line) >= len(pattern) and line[:len(pattern)] == pattern:
+                print("Installing package: " + package)
+                call(manager["install"].split() + [package])
+
+
+def main(args):
+    manager = detect_package_manager()
+    call(manager["update"].split())
+    call(manager["upgrade"].split())
+
+
+if __name__ == "__main__":
+    from sys import argv
+    main(argv[1:])
