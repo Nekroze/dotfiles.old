c328aa9dbdef0d97e04880a70b447e94fa562d32
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-12-17 10:33:57 +1000

    openbox doesnt like symlinks

--- modules/openbox/install.sh
+++ modules/openbox/install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+DOTFILES=~/dotfiles
+
 mkdir -p ~/.config/openbox
 for f in $DOTFILES/modules/openbox/openbox/*; do
     if [ -f ~/.config/openbox/$(basename $f) ]; then
         cp ~/.config/openbox/$(basename $f) ~/.config/openbox/$(basename $f)-original
     fi
-    ln -s $f ~/.config/openbox/$(basename $f)
+    cp $f ~/.config/openbox/$(basename $f)
 done
