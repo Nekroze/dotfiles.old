d42a7e3bea8e563f31e1659f8abc2f38ccf3fa64
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-01-03 20:46:45 +1000

    Added archlinux module

---
+++ modules/archlinux/install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+write() {
+    if ! grep -q $1 $2 ; then
+        touch $2
+        (echo $1; cat $2) >tmpfile
+        mv tmpfile $2
+    fi
+}
+
+echo "Installing package-query"
+curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
+tar zxvf package-query.tar.gz
+cd package-query
+makepkg -si
+cd ..
+rm -rf package-query
+
+echo "Installing yaourt"
+curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
+tar zxvf yaourt.tar.gz
+cd yaourt
+makepkg -si
+cd ..
+rm -rf yaourt
