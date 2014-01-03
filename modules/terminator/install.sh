55fa582755cae7f8bcf3faa6cd2c9efcdef4ced7
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-01-03 18:53:36 +1000

    Added the terminator module for solarized configuration

---
+++ modules/terminator/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p ~/.config/terminator
+if [ -f ~/.config/terminator/config ]; then
+    if [ ! -f ~/.config/terminator/config_original ]; then
+        cp ~/.config/terminator/config ~/.config/terminator/config_original
+    fi
+fi
+cp $DOTFILES/modules/terminator/config ~/.config/terminator/config
