a5c4f190afc5d5e29e22d13cc265a8ca53da833c
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-01-03 18:11:28 +1000

    switched emacsclient script to use only terminal emacs

--- modules/emacs/emacsclient.sh
+++ modules/emacs/emacsclient.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-
-xprop -name emacs >/dev/null 2>/dev/null
-if [ "$?" = "1" ]; then
-	echo "Opening New Frame"
-	emacsclient -a "" -c -n -F "((fullscreen . maximized))" "$@"
-else
-	echo "Using Existing Frame"
-	emacsclient -n "$@"
-fi
+emacsclient -a "" -c -n -t "$@"
