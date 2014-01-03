67a2dd92a6551352df52c3754cc3f813e78ac542
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-01-03 21:02:22 +1000

    added some arch package isntalls

--- modules/archlinux/install.sh
+++ modules/archlinux/install.sh
@@ -23,3 +23,7 @@ cd yaourt
 makepkg -si
 cd ..
 rm -rf yaourt
+
+yaourt -s aurvote
+yaourt -s git wget emacs-nox rxvt-unicode zsh
+yaourt -s python python-virtualenvwrapper python-pip python-tox python-sphinx python2 pypy
