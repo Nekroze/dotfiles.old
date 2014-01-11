7806f9f235f2069859e446a0f1fdcee8107d0203
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-01-12 00:51:20 +1000

    made the git module capable of running silently by dropping name and email config

--- modules/git/install.sh
+++ modules/git/install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-echo
-echo "What is your name?"
-read NAME
-echo "What is your email address?"
-read EMAIL
-git config --global user.name "$NAME"
-git config --global user.email "$EMAIL"
+if [ $SILENT="TRUE" ] ; then
+    echo
+    echo "What is your name?"
+    read NAME
+    echo "What is your email address?"
+    read EMAIL
+    git config --global user.name "$NAME"
+    git config --global user.email "$EMAIL"
+fi
 git config --global --add color.ui true
 git config --global push.default tracking
 git config --global core.whitespace trailing-space,space-before-tab
