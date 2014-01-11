211fedffe952c836e405c498f3255dc481635391
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-01-11 22:31:35 +1000

    Added a new python script to replace the old bash write function

---
+++ write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+
+def main(args):
+    if len(args) != 2:
+        print("Use no more then 2 arguments, a string and a file")
+
+    with open(args[1], 'a+') as f:
+        if args[0] not in f.read():
+            f../write.py(args[0] + "\n")
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
