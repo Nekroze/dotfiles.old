bcc0f8633eee0d7b5fdeb0cde35a7b392454a7cd
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-01-18 11:36:47 +1000

    Switch to normal emacs and noconfirm on yaourt installations

--- modules/arch/install.sh
+++ modules/arch/install.sh
@@ -4,7 +4,7 @@ echo "Installing package-query"
 curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
 tar zxvf package-query.tar.gz
 cd package-query
-makepkg -si
+makepkg -si --noconfirm
 cd ..
 rm -rf package-query
 
@@ -12,7 +12,7 @@ echo "Installing yaourt"
 curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
 tar zxvf yaourt.tar.gz
 cd yaourt
-makepkg -si
+makepkg -si --noconfirm
 cd ..
 rm -rf yaourt
 
@@ -21,5 +21,5 @@ rm -rf yaourt
 ./write.py "TERMINALTITLE=0" ~/.yaourtrc
 
 yaourt -S aurvote
-yaourt -S git wget emacs-nox rxvt-unicode zsh
+yaourt -S git wget emacs rxvt-unicode zsh
 yaourt -S python python-virtualenvwrapper python-pip python-tox python-sphinx python2 pypy
